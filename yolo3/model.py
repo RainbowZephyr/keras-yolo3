0fd713926332509683ee1ae2bed002dc292735cd
RainbowZephyr <darkfeathers9@gmail.com>
2019-12-28 20:49:28 +0200

    modified git ignore

--- yolo3/model.py
+++ yolo3/model.py
@@ -4,6 +4,7 @@ from functools import wraps
 
 import numpy as np
 import tensorflow as tf
+import keras
 from keras import backend as K
 from keras.layers import Conv2D, Add, ZeroPadding2D, UpSampling2D, Concatenate, MaxPooling2D
 from keras.layers.advanced_activations import LeakyReLU
